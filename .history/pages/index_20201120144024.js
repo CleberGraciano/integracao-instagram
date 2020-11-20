73a0263a26dd4877627d2db8b73dc9467188c5d1
CleberGraciano <cleber_batista2010@hotmail.com>
2020-11-20 19:31:19 -0300

    voltando aplicação

---
+++ .history/pages/index_20201120144024.js
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+function Home(){
+    return <div>
+         <h2>Site Para agendamento de postagens em breve :)</h2>
+        <Contador/>
+    </div>
+}
+
+function Contador(){
+    const [contador,setContador]=useState(1);
+    
+    function adicionarContador(){
+        setContador(contador +1 )
+    }
+
+    return(
+        <div>
+        <div>{contador}</div>
+        <button onClick={adicionarContador}>Adicionar Contador</button>
+        <h2><marquee>Patricia</marquee></h2>
+        </div>
+    )
+}
+
+export default Home
